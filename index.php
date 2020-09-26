7f2db1353c441e39699eb70acf2ec6538941f766
kingyeet9048 <sulaimanbada8048@gmail.com>
2020-09-26 18:29:59 -0500

    updating login page

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <?php include('HTML/header.php'); ?>
+    <body>
+        <div class='main'>
+            <header class="center" style="font-size: 30px; text-shadow: 0px 0px 5px rgb(255,255,255); color: white;">WSU CS Project - LedGrid</header>
+            <div id="loginContainer">
+                <input type="text" placeholder="Username" name="username" required>
+                <br>
+                <input type="password" placeholder="Password" name="password" required>
+                <br>
+                <button type="submit">Login</button>
+            </div>
+        </div>
+    </body>
+</html>
