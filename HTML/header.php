5948ef0c33b8262d858f18d1476fec844c5ae796
kingyeet9048 <sulaimanbada8048@gmail.com>
2020-09-26 16:33:28 -0500

    basic layout of login page

---
+++ HTML/header.php
@@ -0,0 +1,6 @@
+<head>
+    <title>LEDGRID</title>
+    <meta charset="UTF-8">
+    <meta name="author" content="Sulaiman Bada">
+    <link rel="stylesheet" type="text/css" href="styles/styles.css">
+</head>
