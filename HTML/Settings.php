35bdecb588835ce83df4c8a4458a1abe28691268
kingyeet9048 <sulaimanbada8048@gmail.com>
2021-04-12 19:41:31 -0500

    made files for marsh and ally.

---
+++ HTML/Settings.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <!-- this links to the header page. All heading info will be loaded into this page. -->
+    <?php include('header.php'); ?>
+    <body>
+        <!-- The navigation bar will be loaded the same way. -->
+        <?php include('navbar.php'); ?>
+        <!-- Will check the url for a valid session id. This will prevent imposters.  -->
+        <?php include('../php/sessionchecker.php'); ?>
+        <!-- Main/Container -->
+        <div id="main" class="slider">
+            <!-- TITLE -->
+            <header class="center" style="font-size: 30px; padding-top: 60px;">WSU CS Project - Settings</header>
+            <div id="content">
+                <a href="../index.php" onclick="remove()">LOGOUT</a>
+                <br>
+                <a href="Profile.php">Profile Information</a>
+                <br>
+                <a href="AllMessages.php">ALL Messages (Admin only)</a>
+            </div>
+        </div>
+    </body>
+</html>
--- HTML/navbar.php
+++ HTML/navbar.php
@@ -10,7 +10,7 @@ That is why its in its own file.  -->
         <li><a href="<?php echo "Background.php".getSession();?>">Background</a></li>
         <li><a href="<?php echo "HowItWorks.php".getSession();?>">How It Works</a></li>
         <li><a href="<?php echo "SendMessage.php".getSession();?>">Send Message</a></li>
-        <li><a href="../index.php" onclick="remove()">LOGOUT</a></li>
+        <li><a href="<?php echo "Settings.php".getSession();?>">Settings</a></li>
         <!-- Start of the session. Meant to keep people from staying on the page
         for too long.  -->
         <script src="../javascript/session.js"></script>
